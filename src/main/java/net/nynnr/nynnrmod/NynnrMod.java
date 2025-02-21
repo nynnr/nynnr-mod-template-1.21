d1276a98411a8ea9183ede00f5ba20ef8d13e42d
nynnr <nynnrsnynnr@gmail.com>
2025-02-21 15:51:45 -0500

    Initial commit

---
+++ src/main/java/net/nynnr/nynnrmod/NynnrMod.java
@@ -0,0 +1,18 @@
+package net.nynnr.nynnrmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NynnrMod implements ModInitializer {
+	public static final String MOD_ID = "nynnr-mod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/nynnr/nynnrmod/NynnrModClient.java
@@ -0,0 +1,10 @@
+package net.nynnr.nynnrmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NynnrModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
